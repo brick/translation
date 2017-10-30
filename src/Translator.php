91371fc75c51b8eae1446adf4bc67e4ecfa2bc78
Benjamin Morel <benjamin.morel@gmail.com>
2017-10-30 15:11:20 +0100

    Do not perform parameter replacement in Translator
    This breaks the single responsibility principle.

--- src/Translator.php
+++ src/Translator.php
@@ -31,20 +31,6 @@ class Translator
     private $dictionaries = [];
 
     /**
-     * An optional string to prepend to parameter keys, such as `[`.
-     *
-     * @var string
-     */
-    private $parameterPrefix = '';
-
-    /**
-     * An optional string to append to parameter keys, such as `]`.
-     *
-     * @var string
-     */
-    private $parameterSuffix = '';
-
-    /**
      * @param TranslationLoader $loader
      */
     public function __construct(TranslationLoader $loader)
@@ -71,27 +57,14 @@ class Translator
     }
 
     /**
-     * @param string $prefix The prefix, such as `[`.
-     * @param string $suffix The suffix, such as `]`.
-     *
-     * @return void
-     */
-    public function setParameterPrefixSuffix($prefix, $suffix)
-    {
-        $this->parameterPrefix = $prefix;
-        $this->parameterSuffix = $suffix;
-    }
-
-    /**
-     * @param string      $key        The translation key to look up.
-     * @param array       $parameters An associative array of parameters to replace in the translated string.
-     * @param string|null $locale     The locale to translate in, or null to use the default locale.
+     * @param string      $key    The translation key to look up.
+     * @param string|null $locale The locale to translate in, or null to use the default locale.
      *
      * @return string
      *
      * @throws \Exception
      */
-    public function translate($key, array $parameters = [], $locale = null)
+    public function translate($key, $locale = null)
     {
         if ($locale === null) {
             if ($this->defaultLocale === null) {
@@ -111,36 +84,6 @@ class Translator
             return $key;
         }
 
-        $value = $dictionary[$key];
-
-        if ($parameters) {
-            return $this->replaceParameters($value, $parameters);
-        }
-
-        return $value;
-    }
-
-    /**
-     * Replaces parameters in a string.
-     *
-     * This is called internally by `translate()`, but is exposed as a public method to allow
-     * more advanced uses such as replacing parameters after executing a transformation on a
-     * translated string.
-     *
-     * @param string $string
-     * @param array  $parameters
-     *
-     * @return string
-     */
-    public function replaceParameters($string, array $parameters)
-    {
-        $placeholders = [];
-
-        foreach ($parameters as $key => $value) {
-            $key = $this->parameterPrefix . $key . $this->parameterSuffix;
-            $placeholders[$key] = $value;
-        }
-
-        return strtr($string, $placeholders);
+        return $dictionary[$key];
     }
 }
