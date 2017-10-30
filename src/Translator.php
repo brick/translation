5d2fe55a4c0f605cf9037d9fb9d34c8a59557e90
Benjamin Morel <benjamin.morel@gmail.com>
2017-10-30 23:39:04 +0100

    Remove default locale from Translator

--- src/Translator.php
+++ src/Translator.php
@@ -17,13 +17,6 @@ class Translator
     private $loader;
 
     /**
-     * The locale to use if none is given in `translate()`.
-     *
-     * @var string|null
-     */
-    private $defaultLocale = null;
-
-    /**
      * An associative array of locale to dictionary.
      *
      * Each dictionary is itself an associative array of keys to texts.
@@ -41,47 +34,19 @@ class Translator
     }
 
     /**
-     * @param string|null $defaultLocale The new default locale, or null to remove it.
-     *
-     * @return void
-     */
-    public function setDefaultLocale(?string $defaultLocale) : void
-    {
-        $this->defaultLocale = $defaultLocale;
-    }
-
-    /**
-     * @return string|null
-     */
-    public function getDefaultLocale() : ?string
-    {
-        return $this->defaultLocale;
-    }
-
-    /**
-     * @param string      $key    The translation key to look up.
-     * @param string|null $locale The locale to translate in, or null to use the default locale.
+     * @param string $key    The translation key to look up.
+     * @param string $locale The locale to translate in, or null to use the default locale.
      *
      * @return string|null The translated string, or null if not found.
      *
      * @throws \Exception
      */
-    public function translate(string $key, string $locale = null) : ?string
+    public function translate(string $key, string $locale) : ?string
     {
-        if ($locale === null) {
-            if ($this->defaultLocale === null) {
-                throw new \Exception('No default locale has been set.');
-            }
-
-            $locale = $this->defaultLocale;
-        }
-
         if (! isset($this->dictionaries[$locale])) {
             $this->dictionaries[$locale] = $this->loader->load($locale);
         }
 
-        $dictionary = $this->dictionaries[$locale];
-
-        return $dictionary[$key] ?? null;
+        return $this->dictionaries[$locale][$key] ?? null;
     }
 }
